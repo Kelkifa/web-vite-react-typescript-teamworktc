195f161704da41a2d5315c882af03c0e0bfc921a
Huân Phạm Long <18520788@ms.uit.edu.vn>
2022-04-21 21:21:07 +0700

    update wed descript tion

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -3,27 +3,36 @@ function HomePage() {
 		<div className="container bg-bgColor p-5">
 			<h1 className="text-center font-bold text-2xl mb-3">Tổng Quan</h1>
 			<div>
-				Ứng dụng hỗ trợ tạo danh sách các sự kiện để những người dùng trong nhóm
-				có thể xem và xử lý các sự kiện của mình. Mỗi sự kiện sẽ chứa danh sách
-				các công việc (todo list) giúp bạn dễ dàng biết được các công việc đã
-				làm hay chưa làm trong sự kiện đó
+				Ứng dụng web giúp bạn tạo và thao tác với các sự kiện được hiển thị trên
+				lịch với nhiều màu sắc khác nhau mà bạn có thể mời những người khác cùng
+				xem và thao tác với chúng.
+				<br />
+				<br />
+				Ứng với mỗi sự kiện trên lịch sẽ có danh sách các công việc của sự kiện
+				đó. Bạn có thể thay đổi trạng thay công việc để đánh dấu là đã thực hiện
+				hoặc ngược lại.
+				<br />
+				<br />
+				Ứng dụng giúp bạn quản lý các sự kiện của bản thân tốt hơn cũng như dễ
+				dàng tìm lại chúng.
 			</div>
 
 			<h1 className="font-bold mb-2 text-xl mt-3">Hướng dẫn</h1>
 			<h2 className="font-bold mb-2 text-lg">Nhóm</h2>
 			<div>
-				Những thành viên trong nhóm mới xem và thao tác được với các Todos và
-				Docs của tài liệu trong nhóm đó. <br />
+				Nơi mà nhưng người dùng có thể chia sẻ các sự kiện với nhau và cùng thao
+				tác với chúng. Bạn cần phải đăng nhập để có thể tạo, tham gia, mời, ...
+				<br />
 				Riêng với nhóm demo, tất cả người dùng đều thuộc vào nhóm này, họ có thể
-				xem và thao tác với các Sự kiện mà không cần đăng nhập
+				xem và thao tác với các Sự kiện mà không cần đăng nhập.
 			</div>
 
 			<h2 className="font-bold mb-2 text-lg mt-3">Sự Kiện</h2>
 			<div>
-				Danh sách các sự kiện được hiển thị trong lịch dựa vào mốc thời gian của
-				sự kiện đó. <br />
+				Danh sách các sự kiện được hiển thị trên lịch dựa vào thời gian bắt đầu
+				và kết thúc của sự kiện đó. <br />
 				Mỗi sự kiện sẽ có một danh sách các công việc có khả năng thay đổi trạng
-				thái xong hoặc đã xong
+				thái đánh dấu công việc đã hoàn thành hoặc chưa.
 			</div>
 		</div>
 	);
